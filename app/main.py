3a7522e069b3aa37669a76eedd8c1e58b370b057
Sven Wolff <sven.wolff@p3-group.com>
2025-01-22 09:26:53 +0100

    added Docker support and uvicorn

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import Union
+from os import environ
 
 from fastapi import FastAPI
 
@@ -8,3 +8,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+if __name__ == "__main__":
+    port = int(environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
